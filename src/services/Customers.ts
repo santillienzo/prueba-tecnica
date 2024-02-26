51d42614da19c973b71b2071db03847e01daafd1
santillienzo <enzo135246@gmail.com>
2024-02-25 21:21:56 -0300

    feat: using fetch

--- src/services/Customers.ts
+++ src/services/Customers.ts
@@ -1,11 +1,10 @@
-import axios from "axios"
 import { Customer } from "../types/customers.types"
 
 const URI = 'https://jsonplaceholder.typicode.com'
 
 //Función para realizar el fetching de los datos de los clientes
 export const fetchCustomers = async (): Promise<Customer[]>=>{
-    return await axios.get<Customer[]>(`${URI}/users`)
-    .then(response => response.data)
+    return fetch(`${URI}/users`)
+    .then(response => response.json())
     .catch(response=> response)
 }
